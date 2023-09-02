eb3646144f232f121ac14e046fb8f1ea61b02c82
Kip Cole <kipcole9@gmail.com>
2023-09-02 23:49:08 +1000

    Parse test times as times

--- test/tempo/iso8601/parse_data_test.exs
+++ test/tempo/iso8601/parse_data_test.exs
@@ -3,21 +3,32 @@ defmodule Tempo.Iso8601.Parser.Data.Test do
 
   @data "./test/support/data/date_test_values_iso.txt"
 
-  @date_tests @data
+  @all_tests @data
   |> File.read!()
   |> String.split("\n")
   |> Enum.with_index()
   |> Enum.reject(&String.starts_with?(elem(&1,0), "#"))
 
+  @time_tests @data
+  |> File.read!()
+  |> String.split("# Times")
+  |> List.last()
+  |> String.split("# Date-Times")
+  |> List.first()
+  |> String.split("\n")
+  |> Enum.with_index()
+  |> Enum.reject(&String.starts_with?(elem(&1,0), "#"))
+  |> Enum.reject(&(elem(&1,0) == ""))
+
   @time_only_test [126, 136, 146, 159]
 
-  for {test_date, line} <- @date_tests, line not in @time_only_test do
+  for {test_date, line} <- @all_tests, line not in @time_only_test do
     test "line #{line}: parse #{inspect test_date}" do
       assert {:ok, _} = Tempo.from_iso8601(unquote(test_date))
     end
   end
 
-  for {test_time, line} <- @date_tests, line in @time_only_test do
+  for {test_time, line} <- @time_tests do
     test "line #{line}: parse as time #{inspect test_time}" do
       assert {:ok, _, "", _, _, _} = Tempo.Iso8601.Tokenizer.time_parser(unquote(test_time))
     end
